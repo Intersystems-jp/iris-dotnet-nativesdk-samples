6de1a51c3a2b146bc5ea934517f82fd0a8416675
wolfman0719 <hsato0719@icloud.com>
2025-10-06 18:00:20 +0900

    Add files via upload

---
+++ dotnet/adbkglobaliterator/adbk-globalnode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using InterSystems.Data.IRISClient;
+using InterSystems.Data.IRISClient.ADO;
+
+namespace samples
+{
+    class AdbkGlobalIterator
+    {
+        static void Main(string[] args)
+        {
+            // Create a connection to IRIS
+            //IRISConnection conn = new IRISConnection();
+            IRISConnection conn = new();
+            IRIS iris;
+
+            // IRIS server Connection Information
+            // Set Server to your IP address and port to IRIS SuperServer port, Log File is optional
+            conn.ConnectionString = "Server = localhost; Port=1972; Namespace=USER; Password = SYS; User ID = _SYSTEM;";
+
+            //Open a Connection to IRIS
+            conn.Open();
+
+            iris = IRIS.CreateIRIS(conn);
+
+            iris.ClassMethodString("Samples.ADBK", "Init");
+            IRISIterator adbkIter = (IRISIterator)iris.GetIRISIterator("Samples.ADBKD");
+            adbkIter.StartFrom("");
+
+            foreach (byte[] record in adbkIter)
+            {
+              IRISList adbkrecord = new(record, record.Length);  
+              Console.WriteLine("Name = " + adbkrecord.Get(2));
+              Console.WriteLine("Telno = " + System.Text.Encoding.ASCII.GetString(adbkrecord.GetBytes(4)));
+              Console.WriteLine("Prefecture = " + adbkrecord.Get(5));
+              Console.WriteLine("Zipcode = " + adbkrecord.Get(3).ToString());
+            }
+            
+            conn.Close();
+        }
+    }
+}
