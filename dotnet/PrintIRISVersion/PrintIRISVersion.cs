3345b6689f01da3be5dd02a6ff59daebcf24040f
wolfman0719 <hsato0719@icloud.com>
2025-10-21 11:52:10 +0900

    Update PrintIRISVersion.cs

--- dotnet/PrintIRISVersion/PrintIRISVersion.cs
+++ dotnet/PrintIRISVersion/PrintIRISVersion.cs
@@ -5,49 +5,56 @@ using InterSystems.Data.IRISClient.ADO;
 
 namespace samples
 {
-    class PrintIRISVersion
+  class PrintIRISVersion
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      try
+      {
+        // Create a connection to IRIS
+        //IRISConnection conn = new IRISConnection();
+        IRISConnection conn = new();
+        IRIS iris;
+
+        // IRIS server Connection Information
+        // Set Server to your IP address and port to IRIS SuperServer port, Log File is optional
+        conn.ConnectionString = "Server = localhost; Port=1972; Namespace=USER; Password = SYS; User ID = _SYSTEM;";
+
+        //Open a Connection to IRIS
+        conn.Open();
+
+        iris = IRIS.CreateIRIS(conn);
+
+        string ReturnValue = iris.ClassMethodString("%SYSTEM.Version", "GetVersion");
+
+        Console.WriteLine("ReturnValue = " + ReturnValue);
+
+        string curDirectory = System.Environment.CurrentDirectory;
+
+        string delim = "";
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            // Create a connection to IRIS
-            //IRISConnection conn = new IRISConnection();
-            IRISConnection conn = new();
-            IRIS iris;
-
-            // IRIS server Connection Information
-            // Set Server to your IP address and port to IRIS SuperServer port, Log File is optional
-            conn.ConnectionString = "Server = localhost; Port=1972; Namespace=USER; Password = SYS; User ID = _SYSTEM;";
-
-            //Open a Connection to IRIS
-            conn.Open();
-
-            iris = IRIS.CreateIRIS(conn);
-
-            string ReturnValue = iris.ClassMethodString("%SYSTEM.Version", "GetVersion");
-
-            Console.WriteLine("ReturnValue = " + ReturnValue);
-            
-            string curDirectory = System.Environment.CurrentDirectory;
-            
-            string delim = "";
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-              delim = "¥";    
-            }
-            else {
-              delim = "/";    
-            }
-            
-            string substring = delim + "dotnet" + delim;
-            
-            string fileName = curDirectory.Split(substring)[0] + delim + "src" + delim + "Samples" + delim + "ADBK.cls";
-            
-            Console.WriteLine("fileName = " + fileName);
-            var status = iris.ClassMethodObject("%SYSTEM.OBJ", "Import", fileName, "ck");
-            Console.WriteLine("status = " + status);
-
-            conn.Close();
+          delim = "¥";
         }
+        else
+        {
+          delim = "/";
+        }
+
+        string substring = delim + "dotnet" + delim;
+
+        string fileName = curDirectory.Split(substring)[0] + delim + "src" + delim + "Samples" + delim + "ADBK.cls";
+
+        Console.WriteLine("fileName = " + fileName);
+        iris.ClassMethodStatusCode("%SYSTEM.OBJ", "Import", fileName, "ck");
+
+        conn.Close();
+      }
+      catch (Exception e)
+      {
+        Console.Write($"error: '{e}'");                     
+      }      
     }
+  }
 }
